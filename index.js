103adfc4517589663216164f84f48a94a72c2b58
Vinh <nguyenvinhhoang2000@gmail.com>
2022-09-08 19:46:34 +0700

    deploy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const authRouter = require("./routes/auth");
 const connecDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kduvejb.mongodb.net/test`,
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@om-db.r30wkcc.mongodb.net/OM`,
       {
         useUnifiedTopology: true,
       }
