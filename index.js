6bc0c333b02b6dd55932054e45f75c020aa734a4
Hoàng Nguyễn Vĩnh <nguyenvinhhoang2000@gmail.com>
2022-12-05 12:20:30 +0700

    update

--- index.js
+++ index.js
@@ -43,12 +43,6 @@ app.use(express.json());
 
 // app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 // parse application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
