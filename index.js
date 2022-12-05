222379cb07e49a28e90717054fb3f6b65846a5fe
Hoàng Nguyễn Vĩnh <nguyenvinhhoang2000@gmail.com>
2022-12-05 12:48:35 +0700

    update

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", async (req, res) => {
+app.get("/product", async (req, res) => {
   res.json("HOME")
 })
 
